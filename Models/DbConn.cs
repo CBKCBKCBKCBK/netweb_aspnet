0442031921b547087a07b8c22929988da21c9819
Bohong.Chen <cbkcbkcbkcbk@yahoo.com.tw>
2023-09-14 21:57:47 +0800

    加入專案檔案。

---
+++ Models/DbConn.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace netweb.Models
+{
+    public class DbConn
+    {
+        private readonly string _ConnStr 
+=ConfigurationManager.ConnectionStrings["MySqlConnection"].ConnectionString;
+    }
+}
